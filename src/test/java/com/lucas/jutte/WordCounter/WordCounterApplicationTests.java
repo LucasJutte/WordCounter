0a3c66d6dfd7d700d954358b97c313f06d33afab
ljutte <lucas@lucasjutte.com>
2023-05-07 22:03:11 +0200

    Commit for spring initializr project

---
+++ src/test/java/com/lucas/jutte/WordCounter/WordCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.jutte.WordCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
